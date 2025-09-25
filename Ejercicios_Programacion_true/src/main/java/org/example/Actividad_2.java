bdf199962f7329b5da15d73e360019e9ad11bccb
A709\dam1 <adrigeada@gmail.com>
2025-09-25 19:05:04 +0200

    Actividad 2. Ejercicio sumar en java

--- Ejercicios_Programacion_true/src/main/java/org/example/Actividad_2.java
+++ Ejercicios_Programacion_true/src/main/java/org/example/Actividad_2.java
@@ -1,4 +1,35 @@
 package org.example;
+import java.sql.SQLOutput;
+import java.util.Scanner;
 
 public class Actividad_2 {
+
+    static void main() {
+
+        Scanner teclado = new Scanner(System.in);
+
+        System.out.println("¿Cómo te llamas?");
+        String nombre = teclado.nextLine();
+
+        System.out.println("¿A que curso vas?");
+        String curso = teclado.next();
+
+        //next y nextline sirven las dos para texto.
+
+        System.out.println("Hola " + nombre + " bienvenid@ al curso " + curso);
+
+        System.out.println("Introduce un número:");
+        int num1 = teclado.nextInt();
+
+        System.out.println("Introduce otro número:");
+        int num2 = teclado.nextInt();
+
+        // opción 1
+        System.out.println("El resultado es " + (num1+num2));
+
+        //opción 2
+        int resultado = num1 + num2;
+        System.out.println("El resultado es " + resultado);
+
+    }
 }
