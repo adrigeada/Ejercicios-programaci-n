0464df52622882d9b63031d266d4dd3224ef31b3
A709\dam1 <adrigeada@gmail.com>
2025-10-02 19:21:33 +0200

    Práctica 4. Multiplicación de 3 cifras

--- Ejercicios_Programacion_true/src/main/java/org/example/Práctica_4.java
+++ Ejercicios_Programacion_true/src/main/java/org/example/Práctica_4.java
@@ -5,11 +5,26 @@ import java.util.Scanner;
 public class Práctica_4 {
     static void main() {
         Scanner teclado = new Scanner(System.in);
+        int num1 = 0;
+        int num2 = 0;
 
-        System.out.println("Introduce el multiplicando (3 cifras)");
-        int num1 = teclado.nextInt();
-        System.out.println("Introduce el multiplicador (3 cifras)");
-        int num2 = teclado.nextInt();
+        do {
+            System.out.println("Introduce el multiplicando (3 cifras)");
+            num1 = teclado.nextInt();
+            if (num1 <= 0 || num1 < 100 || num1 > 999){
+                System.out.println("Error el número tiene que ser de 3 cifras");
+            }
+        }while (num1 <= 0 || num1 < 100 || num1 > 999);
+
+        do {
+            System.out.println("Introduce el multiplicador (3 cifras)");
+            num2 = teclado.nextInt();
+            if (num2 <= 0 || num2 < 100 || num2 > 999){
+                System.out.println("Error el número tiene que ser de 3 cifras");
+            }
+
+        }while (num2 <= 0 || num2 < 100 || num2 > 999);
+
 
         String numero_string = Integer.toString(num2);
         String unidades = numero_string.substring(2);
@@ -22,7 +37,6 @@ public class Práctica_4 {
         String numero_string3 = Integer.toString(num2);
         String centenas = numero_string.substring(1);
         int centenas_int = Integer.parseInt(centenas);
-
 
 
         System.out.println("El producto de la multiplicación es: " +num1 * num2);
